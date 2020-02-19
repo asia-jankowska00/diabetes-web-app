8da27b1d8a318eb2c4fc20553641b1b1e9c3a654
Miroslav-H <mh.elvete@gmail.com>
2020-02-19 13:03:30 +0100

    Push animation v1

---
+++ assets/shopping_guide.js
@@ -0,0 +1,33 @@
+const ClickListener1 = document.getElementById('ClickListener-1');
+const ClickListener2 = document.getElementById('ClickListener-2');
+const ClickListener3 = document.getElementById('ClickListener-3');
+const slides1 = document.getElementById('slides-1');
+const slides2 = document.getElementById('slides-2');
+const slides3 = document.getElementById('slides-3');
+const card1 = document.getElementById('card-1');
+const card2 = document.getElementById('card-2');
+const card3 = document.getElementById('card-3');
+const arrow1 = document.getElementById('arrow-1');
+
+ClickListener1.addEventListener('click', function(){
+	slides1.classList.toggle('hidden');
+	slides1.style.transition = "all .5s ease-in-out";
+	card1.classList.toggle('card-minimized');
+	card1.style.transition = "all .5s ease-in-out";
+	arrow1.classList.toggle('arrow-turn');
+	arrow1.style.transition = "all .3s";
+})
+
+ClickListener2.addEventListener('click', function(){
+	slides2.classList.toggle('hidden');
+	slides2.style.transition = "all .5s ease-in-out";
+	card2.classList.toggle('card-minimized');
+	card2.style.transition = "all .5s ease-in-out";
+})
+
+ClickListener3.addEventListener('click', function(){
+	slides3.classList.toggle('hidden');
+	slides3.style.transition = "all .5s ease-in-out";
+	card3.classList.toggle('card-minimized');
+	card3.style.transition = "all .5s ease-in-out";
+})
