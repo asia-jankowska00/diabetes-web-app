d4eab97c936c5d5f93f6267ec45894096ce1760c
Miroslav-H <mh.elvete@gmail.com>
2020-02-19 13:47:40 +0100

    Fix js->css

--- assets/shopping_guide.js
+++ assets/shopping_guide.js
@@ -14,25 +14,20 @@ const arrow3 = document.getElementById('arrow-3');
 ClickListener1.addEventListener('click', function(){
 	slides1.classList.toggle('hidden');
 	card1.classList.toggle('card-minimized');
-	card1.style.transition = "all .5s ease-in-out";
 	arrow1.classList.toggle('arrow-turn');
 	arrow1.style.transition = "all .3s";
 })
 
 ClickListener2.addEventListener('click', function(){
 	slides2.classList.toggle('hidden');
-	slides2.style.transition = "all .5s ease-in-out";
 	card2.classList.toggle('card-minimized');
-	card2.style.transition = "all .5s ease-in-out";
 	arrow2.classList.toggle('arrow-turn');
 	arrow2.style.transition = "all .3s";
 })
 
 ClickListener3.addEventListener('click', function(){
 	slides3.classList.toggle('hidden');
-	slides3.style.transition = "all .5s ease-in-out";
 	card3.classList.toggle('card-minimized');
-	card3.style.transition = "all .5s ease-in-out";
 	arrow3.classList.toggle('arrow-turn');
 	arrow3.style.transition = "all .3s";
 })
