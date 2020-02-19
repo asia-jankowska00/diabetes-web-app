c3a6c6b314f5add22fec9a0214d44ea705676bc0
Joanna Jankowska <1081578@ucn.dk>
2020-02-19 13:42:11 +0100

    Update shoppinglist.js

--- assets/shoppinglist.js
+++ assets/shoppinglist.js
@@ -1,5 +1,6 @@
 let shoppingList = {
     selectElements: function() {
+        // select elements from shopping list page
        this.listInputField = document.getElementById('input-item-field');
        this.addItemBtn = document.getElementById('add-item-btn');
        this.clearListBtn = document.getElementById('clear-list-btn');
@@ -9,7 +10,14 @@ let shoppingList = {
     createItem: function() {
         // create li element with attributes, value, checkbox, and delete button
         let shoppingListItem, shoppingListItemCheckbox, shoppingListItemValue, shoppingListItemDeleteBtn;
+        // li
         shoppingListItem = document.createElement('li');
         shoppingListItem.setAttribute('class', 'shopping-list-item');
+        // checkbox
+        shoppingListItemCheckbox = document.createElement('input');
+        shoppingListItemCheckbox.setAttribute('type', 'checkbox');
+        // text  value
+        shoppingListItemValue = console.log(document.createTextNode(this.listInputField.value));
+    
     }
 }
