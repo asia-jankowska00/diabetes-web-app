da3e948144165100fcde8e3f8b6520ad5ed5c157
Miroslav-H <mh.elvete@gmail.com>
2020-02-20 22:26:43 +0100

    Comments and rearranging code

---
+++ assets/loading-animation.js
@@ -0,0 +1,8 @@
+const loadScreen = document.getElementById('load-screen')
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    // setTimeout(function () {
+	loadScreen.classList.add('hidden');
+	    // }, 3000)
+});
