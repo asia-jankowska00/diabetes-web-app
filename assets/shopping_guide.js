dec81df93f74e7668ccc571bf7410e7d1310cfdc
Miroslav-H <mh.elvete@gmail.com>
2020-02-24 22:08:34 +0100

    Carousel

--- assets/shopping_guide.js
+++ assets/shopping_guide.js
@@ -67,7 +67,7 @@ let cards = document.getElementsByClassName('card');
 let animationContainer = document.getElementsByClassName('animation-container');
 let arrow = document.getElementsByClassName('arrow-down');
 let swipe = document.getElementsByClassName('block');
-
+let count = 1; // slide counter (used lower)
 
 for (let i = 0; i < clickableTabs.length; i++) {
 	clickableTabs[i].addEventListener('click', function() {
@@ -76,6 +76,7 @@ for (let i = 0; i < clickableTabs.length; i++) {
 		// 		clickableTabs[i].nextElementSibling.classList.toggle('hidden')
 		// 	}
 		// }
+		count = 1;
 		arrow[i].classList.toggle('arrow-turn')
 		animationContainer[i].classList.toggle('hidden')
 		cards[i].classList.toggle('card-minimized')
@@ -93,6 +94,7 @@ for (let i = 0; i < clickableTabs.length; i++) {
 
 // SLIDESHOW 
 
+/*
 let cardIndex = [1,1,1,1,1,1];
 let cardId = ["slideshow-1", "slideshow-2", "slideshow-3", "slideshow-4", "slideshow-5", "slideshow-6"]
 displayCard(1, 0);
@@ -116,9 +118,203 @@ function displayCard(n, no) {
   }
   x[cardIndex[no]-1].style.display = "block";  
 }
+*/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+arrowRight.addEventListener('click', function(){
+	
+	if (count == 4){
+		alert('4');
+		count = 1;
+	}
+	
+	else if (count = 0){
+		count = count+1;
+	}
+	
+	else if (count = 1){
+		count = count+1;
+	}
+	
+	else if (count = 2){
+		count = count+1;
+	}
+	
+	
+	
+});
+*/
+
+const arrowNext = document.getElementsByClassName('arrow-next'); //all next arrows in an array
+const arrowNextNumber = arrowNext.length; //length of the arrow next array
+let slide1;
+let slide2;
+let slide3;
+let slide4;
+var siblings;
+	
+
+	for (let i = 0; i < arrowNextNumber; i++){
+		arrowNext[i].addEventListener('click', function(){
+			let parent = arrowNext[i].parentNode;
+			siblings = parent.childNodes; //stores an array of all the sibling elements of the arrow
+console.log(siblings)
+		
+		let length = siblings.length;
+// 		alert(length);
+
+				
+		// [1 = arrow next, 3 = arrow back, 7 <-> 11 = images] (odd numbers)
+		
+		//images
+	
+/*
+		slide1 = siblings[5];
+ 		console.log(siblings);
+		slide2 = siblings[7];
+		slide3 = siblings[9];
+		slide4 = siblings[11];
+		
+*/
+		if(length == 9){
+			
+			slide1 = siblings[5];
+			slide2 = siblings[7];
+			
+			switch(count){
+				case 1: {
+					count = count + 1;
+					slide1.classList.remove('slide2');
+		 			slide1.classList.add('slide');
+			 		slide2.classList.add('slide2');
+			 		slide2.classList.remove('hidden-image');
+			 		setTimeout(function(){slide1.classList.add('hidden-image')}, 1500);
+			 		slide1.style.zIndex = '0';
+				break};
+				case 2: {
+					count = 1;
+					slide1.classList.remove('slide');
+			 		slide2.classList.remove('slide2');
+					slide2.classList.add('slide');
+			 		slide1.classList.add('slide2');
+			 		slide1.classList.remove('hidden-image');
+			 		setTimeout(function(){slide2.classList.add('hidden-image')}, 1500);
+			 		slide1.style.zIndex = '9';
+				break};
+			};
+		}
+		else if(length == 11){
+			
+			slide1 = siblings[5];
+			slide2 = siblings[7];
+			slide3 = siblings[9];
+			
+			switch(count){
+				case 1: {
+					count = count + 1;
+					slide1.classList.remove('slide2');
+		 			slide1.classList.add('slide');
+			 		slide2.classList.add('slide2');
+			 		slide2.classList.remove('hidden-image');
+			 		setTimeout(function(){slide1.classList.add('hidden-image')}, 1500);
+			 		slide1.style.zIndex = '0';
+				break};
+				case 2: {
+					count = count + 1;
+					slide1.classList.remove('slide');
+			 		slide2.classList.remove('slide2');
+					slide2.classList.add('slide');
+			 		slide3.classList.add('slide2');
+			 		slide3.classList.remove('hidden-image');
+			 		slide1.classList.add('hidden-image');
+			 		setTimeout(function(){slide2.classList.add('hidden-image')}, 1500);
+				break};
+				case 3: {
+					count = 1;
+					slide2.classList.remove('slide');
+			 		slide3.classList.remove('slide2');
+					slide3.classList.add('slide');
+			 		slide1.classList.add('slide2');
+			 		slide1.classList.remove('hidden-image');
+			 		slide2.classList.add('hidden-image');
+			 		setTimeout(function(){slide3.classList.add('hidden-image')}, 1500);
+			 		slide1.style.zIndex = '9';
+				break};
+			};
+		}
+		else if(length == 13){
+			
+			slide1 = siblings[5];
+			slide2 = siblings[7];
+			slide3 = siblings[9];
+			slide4 = siblings[11];
+			
+			switch(count){
+			case 1: {
+				count = count + 1;
+				slide4.classList.remove('slide');
+				slide1.classList.remove('slide2');
+	 			slide1.classList.add('slide');
+		 		slide2.classList.add('slide2');
+		 		slide2.classList.remove('hidden-image');
+		 		slide4.classList.add('hidden-image');
+		 		setTimeout(function(){slide1.classList.add('hidden-image')}, 1500);
+		 		slide1.style.zIndex = '0';
+			break};
+			case 2: {
+				count = count + 1;
+				slide1.classList.remove('slide');
+		 		slide2.classList.remove('slide2');
+				slide2.classList.add('slide');
+		 		slide3.classList.add('slide2');
+		 		slide3.classList.remove('hidden-image');
+		 		slide1.classList.add('hidden-image');
+		 		setTimeout(function(){slide2.classList.add('hidden-image')}, 1500);
+			break};
+			case 3: {
+				count = count + 1;
+				slide2.classList.remove('slide');
+		 		slide3.classList.remove('slide2');
+				slide3.classList.add('slide');
+		 		slide4.classList.add('slide2');
+		 		slide4.classList.remove('hidden-image');
+		 		slide2.classList.add('hidden-image');
+		 		setTimeout(function(){slide3.classList.add('hidden-image')}, 1500);
+		 		//slide3.classList.add('hidden-image');
+			break};
+			case 4: {
+				count = 1;
+				slide3.classList.remove('slide');
+		 		slide4.classList.remove('slide2');
+				slide4.classList.add('slide');
+		 		slide1.classList.add('slide2');
+		 		slide1.classList.remove('hidden-image');
+		 		slide3.classList.add('hidden-image');
+		 		setTimeout(function(){slide4.classList.add('hidden-image')}, 1500);
+		 		//slide4.classList.add('hidden-image');
+		 		slide1.style.zIndex = '9';
+			break};
+		    };
+		};
+	});
+};
 
 
 
