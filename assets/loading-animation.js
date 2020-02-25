abd80b0fd109602db8d6d062c1afa8c8ac958ab8
Joanna Jankowska <1081578@ucn.dk>
2020-02-25 10:46:34 +0100

    Add back to recipes button

--- assets/loading-animation.js
+++ assets/loading-animation.js
@@ -1,6 +1,6 @@
 const loadScreen = document.getElementById('load-screen')
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
     // setTimeout(function () {
 	loadScreen.classList.add('hidden');
@@ -9,10 +9,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-
 // on page laod, save 'pageLoadedBefore' to localstorage
+window.addEventListener('load', () => {
+    if(localStorage.getItem('pageLoadedBefore') === null) {
+        // run tutorial
+        localStorage.setItem('pageLoadedBefore', 'true')
+    }
+});
+
 
 //tutorial tooltips
-
+function runTutorial() {
+    let tuorialSlide1, tutorialSlide2, tutorialSlide3
+    // if tutorialSlide1.cross clicked, close this and display tutorialSlide2
+}
 // get pageLoadedBefore from locaStorage
 // if it doesnt exist, perform tutorial
